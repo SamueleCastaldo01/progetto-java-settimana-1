c92e4a83ac4b1dfef924d4b2683a19f455e697c1
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-09-27 10:50:16 +0200

    immagine

--- src/Immagine.java
+++ src/Immagine.java
@@ -1,2 +1,28 @@
-public class Immagine {
+public class Immagine implements ElementoMultimediale {
+    private String titolo;
+    private int luminosita;
+
+    public Immagine(String titolo, int luminosita) {
+        this.titolo = titolo;
+        this.luminosita = luminosita;
+    }
+
+    @Override
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public int aumentaLuminosita() {
+        return luminosita++;
+    }
+
+    public int diminuisciLuminosita() {
+        return luminosita--;
+    }
+
+    public void shiw() {
+        //repeat è un metodo della classe string che ripete quella stringa n volte
+        System.out.println(titolo + " " + "*".repeat(luminosita));
+    }
+
 }
