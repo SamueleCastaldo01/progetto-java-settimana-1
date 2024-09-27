645211cb880cdca03c86c8db7b329e0f0005d302
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-09-27 10:55:13 +0200

    registrazione Audio

---
+++ src/RegistrazioneAudio.java
@@ -0,0 +1,36 @@
+public class RegistrazioneAudio implements Riproducibile{
+    String titolo;
+    private int durata;
+    private int volume;
+
+    public RegistrazioneAudio(String titolo, int durata, int volume) {
+        this.titolo = titolo;
+        this.durata = durata;
+        this.volume = volume;
+    }
+
+    @Override
+    public int getDurata() {
+        return durata;
+    }
+
+    @Override
+    public void play() {
+        for (int i= 0; i < durata; i++) {
+            System.out.println(titolo + "!".repeat(volume));
+        }
+    }
+
+    @Override
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public int alzaVolume() {
+        return volume++;
+    }
+
+    public int abbassaVolume() {
+        return volume--;
+    }
+}
